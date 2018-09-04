327e9eb0cb025ed9b78bc5b40982cdeee6efcfa0
dhodges47 <dhodges@gmail.com>
2018-09-04 11:30:16 -0400

    Added Mathjs for accurate calculation of steering vectors

--- phidgetServer.js
+++ phidgetServer.js
@@ -1,6 +1,8 @@
 const phidget22 = require('phidget22');
 const pubsub = require('pubsub-js');
 const global = require('./constants');
+const math = require('mathjs'); // for accurate math in the steering function
+
 const ch1 = new phidget22.DCMotor();
 const ch2 = new phidget22.DCMotor();
 const ch3 = new phidget22.DCMotor();
@@ -37,33 +39,32 @@ exports.phidgetServer = function () {
         console.log(data);
         var newVelocity = data;
         if (ch1.getAttached() && ch2.getAttached() && ch3.getAttached() && ch4.getAttached()) {
-            ch1.setTargetVelocity(newVelocity / 100);
-            ch2.setTargetVelocity(newVelocity / 100);
-            ch3.setTargetVelocity(newVelocity / 100);
-            ch4.setTargetVelocity(newVelocity / 100);
-            velocity = round(newVelocity / 100), 2;
+            velocity = math.round(math.divide(newVelocity, 100), 2);
+            ch1.setTargetVelocity(velocity);
+            ch2.setTargetVelocity(velocity);
+            ch3.setTargetVelocity(velocity);
+            ch4.setTargetVelocity(velocity);
         }
     });
-    pubsub.subscribe(global.rovervelocity_statusrequest, function (msg, data)
-    {
-            getVelocity();
+    pubsub.subscribe(global.rovervelocity_statusrequest, function (msg, data) {
+        getVelocity();
     });
     pubsub.subscribe(global.roversteering_command, function (msg, data) {
         console.log(data);
         var newVector = data;
         if (newVector != 0) {
-            newVector = newVector / 100;
+            newVector = math.round(math.divide(newVector, 100), 2);
         }
         if (ch1.getAttached() && ch2.getAttached() && ch3.getAttached() && ch4.getAttached()) {
             // ch1 and ch2 are the left wheels
             // ch3 and ch4 are the right wheels
             console.log('NewVector:' + newVector)
-            var leftNewVelocity = 0;
-            var rightNewVelocity = 0;
+            var leftNewVelocity = 0.00;
+            var rightNewVelocity = 0.00;
 
             var ch1Velocity = ch1.getTargetVelocity(); //get left side velocity
             var ch3Velocity = ch3.getTargetVelocity(); // get right side velocity
-            var velocity = ((ch1Velocity > ch3Velocity) ? ch1Velocity : ch3Velocity); // save current velocity 
+            var velocity = ((ch1Velocity > ch3Velocity) ? ch3Velocity : ch1Velocity); // save current velocity based on slowest wheel
             if (newVector == 0) {
                 // go straight
                 ch1.setTargetVelocity(velocity);
@@ -74,18 +75,16 @@ exports.phidgetServer = function () {
             }
             if (newVector > 0) {
                 // turn right
-                leftNewVelocity = (ch1Velocity + newVector) > 1 ? 1 : ch1Velocity + newVector;
-                //ch2NewVelocity = (ch2Velocity - newVector) < -1 ? -1 : ch2Velocity - newVector;
+                leftNewVelocity = math.round(math.add(ch1Velocity + newVector), 2);
+                leftNewVelocity = leftNewVelocity > 1 ? 1 : leftNewVelocity;
                 rightNewVelocity = velocity;
             }
             else {
                 // turn left, newVector is negative
-                //leftNewVelocity = (ch1Velocity + newVector) < -1 ? -1 : ch1Velocity + newVector;
                 leftNewVelocity = velocity;
-                rightNewVelocity = (ch3Velocity - newVector) > 1 ? 1 : ch3Velocity - newVector;
+                rightNewVelocity = math.round(math.add(ch3Velocity - newVector), 2);
+                rightNewVelocity = rightNewVelocity > 1 ? 1 : rightNewVelocity;
             }
-            leftNewVelocity = round(leftNewVelocity, 1);
-            rightNewVelocity = round(rightNewVelocity, 1);
             console.log('left velocity: ' + leftNewVelocity)
             console.log('right velocity: ' + rightNewVelocity)
 
@@ -144,35 +143,11 @@ exports.phidgetServer = function () {
         ch1.open().then(function (ch1) {
             console.log('channel 1 open');
 
-            //var i = ch1.getMaxAcceleration();
-            //console.log('Max Acceleration is ' + i);
-            //var j = ch1.getMaxVelocity();
-            //console.log('Max Velocity is ' + j);
-            //ch1.setAcceleration(10);
-            //ch1.setTargetVelocity(1);
-
         }).catch(function (err) {
             console.log('failed to open the channel:' + err);
-        });
-        //var ix1 = ch1.getAttached();
-        //var ix2 = ch1.getAttached();
-        //if (ix1) {
-        //ch1.close();
-        //console.log('channel 0 closed')
-        //}
-        // if (ix2) {
-        //ch2.close();
-        //console.log('channel 1 closed');
-        // }
+        });     
         ch2.open().then(function (ch2) {
             console.log('channel 2 open');
-            //var i = ch2.getMaxAcceleration();
-            // console.log('Max Acceleration is ' + i);
-            // var j = ch2.getMaxVelocity();
-            // console.log('Max Velocity is ' + j);
-            // ch2.setAcceleration(10);
-            // ch2.setTargetVelocity(1);
-
         }).catch(function (err) {
             console.log('failed to open the channel:' + err);
         });
@@ -188,32 +163,26 @@ exports.phidgetServer = function () {
         }).catch(function (err) {
             console.log('failed to open the channel:' + err);
         });
-
-
     }
     var getVelocity = function () {
         var responseArray = new Array(4);
         if (ch1.getAttached()) {
             velocity = ch1.getTargetVelocity();
             responseArray[0] = velocity;
-           
+
         }
         if (ch2.getAttached()) {
             velocity = ch2.getTargetVelocity();
             responseArray[1] = velocity;
-        } 
+        }
         if (ch3.getAttached()) {
             velocity = ch3.getTargetVelocity();
             responseArray[2] = velocity;
-        } 
+        }
         if (ch4.getAttached()) {
             velocity = ch4.getTargetVelocity();
-           responseArray[3] = velocity;
+            responseArray[3] = velocity;
         }
         pubsub.publish(global.rovervelocity_statusreport, responseArray);
-    }
-    function round(value, precision) {
-        var multiplier = Math.pow(10, precision || 0);
-        return Math.round(value * multiplier) / multiplier;
     }
 }
